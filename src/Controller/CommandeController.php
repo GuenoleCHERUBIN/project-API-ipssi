2169258a7fac69e41be388e3542c846b4585dee1
Abishan13 <arul.abishanpro@gmail.com>
2022-04-06 17:10:16 +0200

    Fixed CommandeController

--- src/Controller/CommandeController.php
+++ src/Controller/CommandeController.php
@@ -13,32 +13,63 @@ final class CommandeController extends DefaultController{
         $this->model = new CommandeModel;
     }
 
-    public function index ()
+    public function index () :never
     {
         $commandes = $this->model->findAll();
-        $this->jsonResponse($commandes, 200);
+        self::jsonResponse($commandes, 200);
     }
 
+     /**
+     * Renvoie une commande en fonction de son idCommande
+     *
+     * @param integer $idCommande
+     * @return void
+     */
     public function single (int $idCommande)
     {
         $commande = $this->model->find($idCommande);
-        $cli = [
-            'id' => $commande->getIdCommande(),
-        ];
-        $this->jsonResponse($cli, 200);
+        self::jsonResponse($commande, 200);
 
     }
 
-    public function save ()
+    /**
+     * Enregistre une commande en BDD
+     *
+     * @param array $data
+     * @return void
+     */
+    public function save (array $data)
     {
-        $lastId = $this->model->save($_POST);
+        $lastId = $this->model->save($data);
         $commande = $this->model->find($lastId);
-        $cli = [
-            'id' => $client->getIdCommande(),
-            'name' => $client->getName()
-        ];
-        $this->jsonResponse($cli, 201);
+        self::jsonResponse($commande, 201);
 
 
+    }
+    /**
+     * Modifie une commande en BDD
+     *
+     * @param integer $idCommande
+     * @param array $data
+     * @return void
+     */
+    public function update (int $idCommande, array $data)
+    {
+        if($this->model->update($idCommande, $data)) {
+            $commande = $this->model->find($idCommande);
+            self::jsonResponse($commande, 201);
+        }
+    }
+
+    /**
+     * Supprime une commande en BDD
+     *
+     * @param integer $idCommande
+     * @return void
+     */
+    public function delete (int $idCommande)
+    {
+        $this->model->delete($idCommande);
+        self::jsonResponse("commande supprimée", 200);
     }
 }
